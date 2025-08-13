8fb6f2e3de27aab40075ae4b065056a6b319e472
brianrivet-tilt <brian@tiltthis.com>
2025-08-13 17:43:09 -0500

    reinstating alpine for javascript and work on tailwind classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,209 @@
 /* CSS */
 import "../css/app.css";
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
 
 /* JS */
 import 'lazysizes';
-
-
+import { gsap } from "gsap";    
+import { CustomEase } from "gsap/CustomEase";
+import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";    
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+/* The following plugins are Club GSAP perks */
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { SplitText } from "gsap/SplitText";
+//import * as YTPlayer from 'yt-player';
 
 /* SCRIPTS HERE */
 
-(function() {
-  // Encapsulated functions
-  function accPanelFunction(btn, state) {    
-    console.log(btn);
-    console.log(state);
-    const btnElem = document.getElementById(btn);
-    const panel = btnElem.nextElementSibling;
-    if (state) {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } else {      
-      panel.style.maxHeight = null;
-    }
-  }
-   
-  // Expose the function to the global scope under a namespace
-  window.tiltBoiler = window.tiltBoiler || {};
-  window.tiltBoiler.accPanelFunction = accPanelFunction;  
-})();
+/**
+ * Alpine JS
+ * Best to put it last so that all other potential JS is available
+ * when components start getting initialized.
+ */
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+import focus from '@alpinejs/focus';
+import persist from '@alpinejs/persist';
 
+var duration = duration || 1;
+gsap.registerPlugin(Flip,ScrollTrigger,ScrollToPlugin,Draggable,MotionPathPlugin,TextPlugin,DrawSVGPlugin,ScrambleTextPlugin,SplitText,RoughEase,ExpoScaleEase,SlowMo,CustomEase);    
+
+document.addEventListener('alpine:init', () => {
+  
+  Alpine.store('contentModal', {
+    on: false,
+    modalHtml: '',
+    showLoading: false,   
+  
+    open(modalUrl, modalTitle) {      
+      this.modalHtml='loading...'; 
+      this.showLoading = true; 
+      this.on = true;
+            
+      fetch(modalUrl, {
+        method: 'GET',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+        },
+      })
+      .then(response => response.text())
+      .then(text => {
+        this.modalHtml = text
+        this.showLoading = false;			   
+      })
+      
+    },
+    
+    close() {
+      this.on = false;
+      this.modalHtml = '';
+      this.showLoading = false;   
+    }
+  });
+  
+  Alpine.data('sectionStandard', (animateContent, staggerAnimations) => ({
+    init() {       
+      if (animateContent) {
+        const animParent = this.$refs.animparent;
+        const animSelector = "#" + animParent.getAttribute('id') + " .animate" ;
+        const animCast = document.querySelectorAll(animSelector);        
+        if (staggerAnimations) {          
+          ScrollTrigger.batch(animCast, {
+            onEnter: batch => gsap.from(batch, 
+            {
+              duration: duration * 1.5,		
+              y: 50,
+              opacity: 0,
+              // Note: Easing Pack required for easing
+              ease: "expo.out",
+              onComplete() {
+                this.postRevealFunction;
+              },
+              stagger: 0.15			
+            }),
+            start: "top 95%",
+            once: true
+          });         
+        } else {
+          animCast.forEach(animItem => {              
+            gsap.from(animItem, { 			
+              duration: duration * 1.5,
+              y: 50,			
+              opacity: 0,              			
+              // Note: Easing Pack required for easing
+              ease: "expo.out",
+              onComplete: this.postRevealFunction,              	
+              scrollTrigger: {
+                trigger: animParent,
+                start: "top 95%",
+              }			
+            })
+          });
+        }
+      } else {
+        const animElem = this.$refs.animate;     
+        gsap.from(animElem, {
+          duration: duration * 1.5,
+          y: 50,          
+          opacity: 0,			
+          // Note: Easing Pack required for easing
+          ease: "expo.out",
+          onComplete: this.postReveal,          			
+          scrollTrigger: {
+            trigger: animElem,
+            start: "top 95%",
+          }
+        })
+      }      
+    },
+        
+    postReveal() {
+      
+    }    
+      
+  })),
+  
+  Alpine.data('sectionSidePhoto', (animateContent, staggerAnimations) => ({
+    init() {       
+      if (animateContent) {
+        const animParent = this.$refs.animparent;
+        const animSelector = "#" + animParent.getAttribute('id') + " .animate" ;
+        const animCast = document.querySelectorAll(animSelector);        
+        if (staggerAnimations) {          
+          ScrollTrigger.batch(animCast, {
+            onEnter: batch => gsap.from(batch, 
+            {
+              duration: duration * 1.5,	              
+              opacity: 0,
+              // Note: Easing Pack required for easing
+              ease: "expo.out",
+              onComplete() {
+                this.postRevealFunction;
+              },
+              stagger: 0.15			
+            }),
+            start: "top 95%",
+            once: true
+          });         
+        } else {
+          animCast.forEach(animItem => {              
+            gsap.from(animItem, { 			
+              duration: duration * 1.5,              			
+              opacity: 0,              			
+              // Note: Easing Pack required for easing
+              ease: "expo.out",
+              onComplete: this.postRevealFunction,              	
+              scrollTrigger: {
+                trigger: animParent,
+                start: "top 95%",
+              }			
+            })
+          });
+        }
+      } else {
+        const animElem = this.$refs.animate;     
+        gsap.from(animElem, {
+          duration: duration * 1.5,          
+          force3D: true,
+          opacity: 0,			
+          // Note: Easing Pack required for easing
+          ease: "expo.out",
+          onComplete: this.postReveal,          			
+          scrollTrigger: {
+            trigger: animElem,
+            start: "top 95%",
+          }
+        })
+      }      
+    },
+        
+    postReveal() {
+      
+    }    
+      
+  }))
+  
+ 
+    
+})
+
+if (typeof window.Alpine === "undefined") {
+  Alpine.plugin(collapse);
+  Alpine.plugin(focus);
+  Alpine.plugin(persist);
+  window.Alpine = Alpine;
+  window.Swiper = Swiper;  
+  Alpine.start();
+}
 
 
 
