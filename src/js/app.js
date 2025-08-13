0991761d44d50ae9aa6f6a3eea619b1f68276fc5
Brian Rivet <brian@odysseydesignstudio.com>
2025-08-12 19:22:54 -0500

    work on accordion

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,26 @@ import 'lazysizes';
 
 /* SCRIPTS HERE */
 
+(function() {
+  // Encapsulated functions
+  function accPanelFunction(btn, state) {    
+    console.log(btn);
+    console.log(state);
+    const btnElem = document.getElementById(btn);
+    const panel = btnElem.nextElementSibling;
+    if (state) {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } else {      
+      panel.style.maxHeight = null;
+    }
+  }
+   
+  // Expose the function to the global scope under a namespace
+  window.tiltBoiler = window.tiltBoiler || {};
+  window.tiltBoiler.accPanelFunction = accPanelFunction;  
+})();
+
+
 
 
 // Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
