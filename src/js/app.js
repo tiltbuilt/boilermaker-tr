2fd88da49c512a3c2dbcc27a4059d5e6f88a6410
brianrivet-tilt <brian@tiltthis.com>
2025-12-18 17:31:52 -0600

    safelist adjustments

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,21 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 /* JS */
-//import 'lazysizes';
-import { animate, scroll, cubicBezier, easeOut, hover, press, inView, resize } from "motion"
+//import { animate, scroll, cubicBezier, easeOut, hover, press, inView, resize } from "motion"
 
+import { gsap } from "gsap";    
+import { CustomEase } from "gsap/CustomEase";
+import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";    
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+/* The following plugins are Club GSAP perks */
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { SplitText } from "gsap/SplitText";
 //import * as YTPlayer from 'yt-player';
 
 /* SCRIPTS HERE */
